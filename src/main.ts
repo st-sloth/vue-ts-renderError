ff92cd5867333d9bfeec147be040e19e18441355
st-sloth <stsloth@protonmail.com>
2018-08-11 08:35:54 +0500

    renderError TS showcase
    10:12 Expected 0 arguments, but got 3.
         8 |   // https://vuejs.org/v2/api/#renderError
         9 |   renderError (h, err) {
      > 10 |     return h('pre', { style: { color: 'red' }}, err.stack)
           |            ^
        11 |   },
        12 | }).$mount('#app')
        13 |

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,9 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  // https://vuejs.org/v2/api/#renderError
+  renderError (h, err) {
+    return h('pre', { style: { color: 'red' }}, err.stack)
+  },
 }).$mount('#app')
